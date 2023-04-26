35bd026c4d9173a95e5ae07c4a84a5042fd9c763
Kemer <kitowski.przemek@gmail.com>
2023-04-26 19:10:26 +0200

    Added view support for very small devices

--- src/pages/home-page/scripts/tracklist-component.js
+++ src/pages/home-page/scripts/tracklist-component.js
@@ -21,41 +21,41 @@ const trackList = [
         duration: '3:52',
         coverImage: '/tracks/gemini-rights.png'
     },
-    // {
-    //     artist: 'Mark Menday, Seb Mount',
-    //     track: 'Talk To Me',
-    //     album: 'Talk To Me',
-    //     duration: '2:28',
-    //     coverImage: '/tracks/talk-to-me.png'
-    // },
-    // {
-    //     artist: 'Olivia Rodrigo',
-    //     track: 'Good 4 u',
-    //     album: 'Sour',
-    //     duration: '2:58',
-    //     coverImage: '/tracks/sour.png'
-    // },
-    // {
-    //     artist: 'Lil Nas X',
-    //     track: 'INDUSTRY BABY',
-    //     album: 'Singiel',
-    //     duration: '3:32',
-    //     coverImage: '/tracks/singiel.png'
-    // },
-    // {
-    //     artist: 'Harry Styles',
-    //     track: 'Watermelon Sugar',
-    //     album: 'Fine Line',
-    //     duration: '2:54',
-    //     coverImage: '/tracks/fine-line.png'
-    // },
-    // {
-    //     artist: 'Isal Daniels',
-    //     track: 'Always',
-    //     album: 'Fine Line',
-    //     duration: '3:33',
-    //     coverImage: '/tracks/yours.png'
-    // },
+    {
+        artist: 'Mark Menday, Seb Mount',
+        track: 'Talk To Me',
+        album: 'Talk To Me',
+        duration: '2:28',
+        coverImage: '/tracks/talk-to-me.png'
+    },
+    {
+        artist: 'Olivia Rodrigo',
+        track: 'Good 4 u',
+        album: 'Sour',
+        duration: '2:58',
+        coverImage: '/tracks/sour.png'
+    },
+    {
+        artist: 'Lil Nas X',
+        track: 'INDUSTRY BABY',
+        album: 'Singiel',
+        duration: '3:32',
+        coverImage: '/tracks/singiel.png'
+    },
+    {
+        artist: 'Harry Styles',
+        track: 'Watermelon Sugar',
+        album: 'Fine Line',
+        duration: '2:54',
+        coverImage: '/tracks/fine-line.png'
+    },
+    {
+        artist: 'Isal Daniels',
+        track: 'Always',
+        album: 'Fine Line',
+        duration: '3:33',
+        coverImage: '/tracks/yours.png'
+    },
 ]
 
 trackList.forEach((track, index) => {
